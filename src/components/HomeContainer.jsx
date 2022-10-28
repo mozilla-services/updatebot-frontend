5e7e971d2150a424197e23295e6f1ee43d6f39f2
Angela Francisco <98919256+angelafrancisco@users.noreply.github.com>
2022-10-28 10:51:00 -0700

    Add basic home page wireframe (#14)
    * removed unnecessary files from cra and installed sass
    
    * ran create-react-app eject scripts
    
    * created base components for home, added protocol
    
    * PR revisions

---
+++ src/components/HomeContainer.jsx
@@ -0,0 +1,10 @@
+const HomeContainer = () => {
+    return (
+        <div className="mzp-l-content">
+            <h3>Testing, testing!</h3>
+            <p>Here is a section of text that may or may not exist later on. Instead of adding lorem ipsum I'm going to talk about how much I love baseball...</p>
+        </div>
+    )
+}
+
+export default HomeContainer;
